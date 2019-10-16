1c5bdc18baf946950d789cb4d49ed2612737c534
jacch <jacch.chen@ivt.com.tw>
2019-10-16 15:55:07 +0800

    commit update comment

--- crud/update.php
+++ crud/update.php
@@ -8,16 +8,16 @@
   連結資料庫->寫入一筆->
 
 ********************************************************************/
+//變數放這裡
 
 
 
 //打開資料庫的　sqlite = 資料庫類型
 //demo.db =資料庫位置
 //如果路徑不在當前的目錄　: /home/pi/demo.db
-//連結資料庫
-    
-//連結資料庫    
+//連結資料庫 
 $myPDO = new PDO('sqlite:/home/pi/laravel/sqlitedb/crud.db');
+
  //更新計數次數
 $myPDO->exec("update score set counter=counter+1 where name='kiki'");
 
