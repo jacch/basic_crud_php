e0a0232aceec122fafbd884a4f1f71b52aae77aa
jacch <jacch.chen@ivt.com.tw>
2019-10-16 15:53:28 +0800

    add default comments

--- crud/create.php
+++ crud/create.php
@@ -1,8 +1,27 @@
 <?php 
+/*** ***************************************************************
+
+  作者 :
+  日期：
+  用途：
+  作法：
+  連結資料庫->寫入一筆->
+
+********************************************************************/
+//變數放這裡
 
 
-$myPDO = new PDO('sqlite:/home/pi/laravel/sqlitedb/crud.db');
+
+
+
+//打開資料庫的　sqlite = 資料庫類型
+//demo.db =資料庫位置
+//如果路徑不在當前的目錄　: /home/pi/demo.db
+
+$myPDO = new PDO('sqlite:demo.db');
+
  //寫入一筆資料
 $myPDO->exec("insert into score values ('kiki','10')");
+
 
 ?>
